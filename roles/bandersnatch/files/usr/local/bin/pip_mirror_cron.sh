983e1a97eba6f011f31800877b2b94f02b559194
Manjunath Ravi <mravi@softlayer.com>
2016-07-19 11:00:57 -0500

    Bandersnatch fine tune

--- roles/bandersnatch/files/usr/local/bin/pip_mirror_cron.sh
+++ roles/bandersnatch/files/usr/local/bin/pip_mirror_cron.sh
@@ -2,13 +2,13 @@
 
 function quit()
 {
-  exit 1
+    echo "Exiting! Since another update process is currently running"
+    exit 1
 }
 
 (
 
    flock -n 9 || quit
-
-/home/bandersnatch/bin/bandersnatch mirror
+   /opt/bandersnatch/bin/bandersnatch mirror
 
 ) 9>/var/lock/bandersnatch
