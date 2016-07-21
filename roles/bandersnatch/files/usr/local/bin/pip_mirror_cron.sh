b14e79d53fe89ab47097d9b2be2d9a44ae31bd84
root <root@mravi3.softlayer.com>
2016-07-21 13:57:05 -0500

    Bandersnatch changes

--- roles/bandersnatch/files/usr/local/bin/pip_mirror_cron.sh
+++ roles/bandersnatch/files/usr/local/bin/pip_mirror_cron.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+FILE_NAME="/var/log/bandersnatch/pip_mirror_cron_job-$(date +%Y-%m-%d-%M).log"
+
 function quit()
 {
-    echo "Exiting! Since another update process is currently running"
+    echo "Exiting! Since another update process is currently running" > $FILE_NAME 2>&1
     exit 1
 }
 
 (
 
    flock -n 9 || quit
-   /opt/bandersnatch/bin/bandersnatch mirror
+   /opt/bandersnatch/bin/bandersnatch mirror > $FILE_NAME 2>&1
 
-) 9>/var/lock/bandersnatch
+) 9>/var/lock/bandersnatch.lck
